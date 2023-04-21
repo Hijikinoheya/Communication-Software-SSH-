b35af80c1c8e083dc0c29c94b6c05a7c4a4246a7
Hisaki714.github.io <87563197+Hisaki714@users.noreply.github.com>
2023-04-21 19:33:54 +0800

    Make File

---
+++ main.py
@@ -0,0 +1,27 @@
+import paramiko
+
+with paramiko.SSHClient() as client:
+    # Server info
+    HOSTNAME = ''
+    USERNAME = ''
+    KEY_FILENAME = ''
+    PASSWORD = ''
+    LINUX_COMMAND = ''
+
+    # Start SSH
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    # Mode of passwd
+    # If you need, comment out to use this problem.
+    # client.connect(hostname=HOSTNAME, port=22, username=USERNAME, password=PASSWORD)
+
+    # SSH keyfile
+    client.connect(hostname=HOSTNAME, port=22, username=USERNAME, key_filename=KEY_FILENAME)
+
+    # thrown command
+    stdin, stdout, stderr = client.exec_command(LINUX_COMMAND)
+
+    # End of connection.
+    for line in stdout:
+        print(line, end='')
